4ec5325aaab239d419165b546d3b371b0494d91f
sai charan <saicharanreddy14396@gmail.com>
2023-01-03 12:24:27 +0530

    print colors

--- 02-print.sh
+++ 02-print.sh
@@ -1,1 +1,18 @@
 echo hello world
+
+echo "**** hello world ****"
+
+# color code syntax : echo -e "\e[COLmMESSAGE\e[0m"
+# red 31
+# green 32
+# yellow 33
+# blue 34
+# magenta 35
+# cyan 36
+
+echo -e "\e[31mMESSAGE\e[0m"
+echo -e "\e[32mMESSAGE\e[0m"
+echo -e "\e[33mMESSAGE\e[0m"
+echo -e "\e[34mMESSAGE\e[0m"
+echo -e "\e[35mMESSAGE\e[0m"
+echo -e "\e[36mMESSAGE\e[0m"
